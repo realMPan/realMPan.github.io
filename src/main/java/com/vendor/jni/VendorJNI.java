f2225f712062505f23c10fa10d55f0bc5cf98bd7
LasagnaFring <michaeljpanny@gmail.com>
2024-04-07 18:32:21 -0400

    More updates and adjustments

--- src/main/java/com/vendor/jni/VendorJNI.java
+++ src/main/java/com/vendor/jni/VendorJNI.java
@@ -1,25 +1,21 @@
 package com.vendor.jni;
 
+import edu.wpi.first.util.RuntimeLoader;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import edu.wpi.first.util.RuntimeLoader;
-
-/**
- * Demo class for loading the driver via JNI.
- */
+/** Demo class for loading the driver via JNI. */
 public class VendorJNI {
   static boolean libraryLoaded = false;
   static RuntimeLoader<VendorJNI> loader = null;
 
-  /**
-   * Helper class for determining whether or not to load the driver on static initialization.
-   */
+  /** Helper class for determining whether or not to load the driver on static initialization. */
   public static class Helper {
     private static AtomicBoolean extractOnStaticLoad = new AtomicBoolean(true);
 
     /**
      * Get whether to load the driver on static init.
+     *
      * @return true if the driver will load on static init
      */
     public static boolean getExtractOnStaticLoad() {
@@ -28,6 +24,7 @@ public class VendorJNI {
 
     /**
      * Set whether to load the driver on static init.
+     *
      * @param load the new value
      */
     public static void setExtractOnStaticLoad(boolean load) {
@@ -38,7 +35,9 @@ public class VendorJNI {
   static {
     if (Helper.getExtractOnStaticLoad()) {
       try {
-        loader = new RuntimeLoader<>("VendorDriver", RuntimeLoader.getDefaultExtractionRoot(), VendorJNI.class);
+        loader =
+            new RuntimeLoader<>(
+                "VendorDriver", RuntimeLoader.getDefaultExtractionRoot(), VendorJNI.class);
         loader.loadLibrary();
       } catch (IOException ex) {
         ex.printStackTrace();
@@ -50,21 +49,23 @@ public class VendorJNI {
 
   /**
    * Force load the library.
+   *
    * @throws java.io.IOException thrown if the native library cannot be found
    */
   public static synchronized void forceLoad() throws IOException {
     if (libraryLoaded) {
       return;
     }
-    loader = new RuntimeLoader<>("VendorDriver", RuntimeLoader.getDefaultExtractionRoot(), VendorJNI.class);
+    loader =
+        new RuntimeLoader<>(
+            "VendorDriver", RuntimeLoader.getDefaultExtractionRoot(), VendorJNI.class);
     loader.loadLibrary();
     libraryLoaded = true;
   }
 
   /**
-   * Tells the driver to initialize.
-   * This is a demo of a native JNI method from the driver.
-   * 
+   * Tells the driver to initialize. This is a demo of a native JNI method from the driver.
+   *
    * @return the int returned by the driver
    * @see "VendorJNI.cpp"
    */
