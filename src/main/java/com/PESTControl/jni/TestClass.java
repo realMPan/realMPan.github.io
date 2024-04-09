ac103bf9c2003b4944f74b987b77ab6e3fb0b060
LasagnaFring <michaeljpanny@gmail.com>
2024-04-09 12:43:35 -0400

    First update to test how to update vendorlibs

---
+++ src/main/java/com/PESTControl/jni/TestClass.java
@@ -0,0 +1,9 @@
+package com.PESTControl.jni;
+
+/**The hell do you think it is? */
+public class TestClass {
+    /** Bingus. */
+    public static void bingus(){
+        System.out.println("bingus");
+    }
+}
