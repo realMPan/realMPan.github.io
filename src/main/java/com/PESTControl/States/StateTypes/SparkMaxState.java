e0b231af975ea78277e633b0478811ee8b47e0bf
LasagnaFring <michaeljpanny@gmail.com>
2024-04-13 11:24:51 -0400

    Hopefully fully working StateMachine State Pathfinding. Going to begin work on rewriting the RobotCode for PESTControl implementation

--- src/main/java/com/PESTControl/States/StateTypes/SparkMaxState.java
+++ src/main/java/com/PESTControl/States/StateTypes/SparkMaxState.java
@@ -1,15 +1,16 @@
-package com.PESTControl.States;
+package com.PESTControl.States.StateTypes;
 
 import java.lang.annotation.Target;
 
 import com.PESTControl.StateMachine.StateMachine;
+import com.PESTControl.States.State;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
-/**An implementation of the default State class meant for implementing states controlled with a CANSparkMax motor controller 
- * Has optional Position and Velocity controller implementation
+/**An extension of the abstract State class meant for implementing states controlled with a CANSparkMax motor controller <p>
+ * Has constructor overloads for FeedForward and PIDController usage for position/velocity-based control.
 */
 public class SparkMaxState extends State {
     private CANSparkMax controller;
@@ -17,6 +18,7 @@ public class SparkMaxState extends State {
     private double currentOutput = 0;
     private SimpleMotorFeedforward motorFeedforward = null;
     private PIDController velocityController;
+    private Runnable controlFunction;
 
 
     /**
@@ -36,7 +38,7 @@ public class SparkMaxState extends State {
         this.controller = controller;
     }
     /**
-     * Creates a SparkMaxState
+     * Creates a SparkMaxState for positional targeting
      * @param name
      * The name of your state, primarily used for debugging
      * @param target
@@ -44,17 +46,22 @@ public class SparkMaxState extends State {
      * @param boundMachine
      * The StateMachine that this state is bound to. The Binded StateMachine is the only thing that will automatically call move()
      * @param controller
-     * The CANSparkMax motor controller this State will manipulate for movement.
+     * The CANSparkMax motor controller this State will manipulate for movement. Its AbsoluteEncoder will be used for position tracking
+     * @param positionController
+     * A PIDController for getting the CANSparkMax to proper position.
      * 
      */
     SparkMaxState(String name, double target, StateMachine boundMachine, CANSparkMax controller, PIDController positionController) {
         super(name, target, boundMachine, () -> {return controller.getAbsoluteEncoder().getPosition();});
-        this.controller = controller;
-        this.positionController = positionController;
-        positionController.setSetpoint(target);
+        controlFunction = () -> {
+            controller.set(
+                positionController.calculate(controller.getAbsoluteEncoder().getPosition(), 
+                target));
+        };
+        overrideControlFunction(controlFunction);
     }
     /**
-     * Creates a SparkMaxState
+     * Creates a SparkMaxState for velocity targeting
      * @param name
      * The name of your state, primarily used for debugging
      * @param target
@@ -62,28 +69,22 @@ public class SparkMaxState extends State {
      * @param boundMachine
      * The StateMachine that this state is bound to. The Binded StateMachine is the only thing that will automatically call move()
      * @param controller
-     * The CANSparkMax motor controller this State will manipulate for movement.
+     * The CANSparkMax motor controller this State will manipulate for movement. Its AbsoluteEncoder will be used for velocity tracking
+     * @param motorFeedforward
+     * A feedforward for reliable controlling the velocity of your motor. Note that the Feedforward will always assume zero acceleration
+     * @param velocityController
+     * A PIDController for error correction in the motors velocity
      * 
      */
     SparkMaxState(String name, double target, StateMachine boundMachine, CANSparkMax controller, SimpleMotorFeedforward motorFeedforward, PIDController velocityController) {
         super(name, target, boundMachine, () -> {return controller.getAbsoluteEncoder().getVelocity();});
-        this.controller = controller;
-        this.motorFeedforward = motorFeedforward;
-        this.velocityController = velocityController;
-        velocityController.setSetpoint(target);
-    }
-
-
-    @Override
-    public void move() {
-        if(positionController == null && velocityController == null){
-          controller.set(target);
-        }else if(velocityController == null){
-            currentOutput = positionController.calculate(currentValueGetter.getAsDouble(), target);
-        }else{
-            currentOutput = motorFeedforward.calculate(target)+velocityController.calculate(currentValueGetter.getAsDouble());
-        }
-        controller.set(currentOutput);
+        controlFunction = () -> {
+            controller.set(
+                motorFeedforward.calculate(controller.getAbsoluteEncoder().getVelocity() + velocityController.calculate(controller.getAbsoluteEncoder().getVelocity(), 
+                target))
+            );
+        };
+        overrideControlFunction(controlFunction);  
     }
     
 }
---
+++ src/main/java/com/PESTControl/SystemStateMachines/RobotStateMachine.java
@@ -0,0 +1,52 @@
+package com.PESTControl.SystemStateMachines;
+
+import java.util.ArrayList;
+
+import com.PESTControl.PESTControlUtilities.StateBinder;
+import com.PESTControl.StateMachine.StateMachine;
+import com.PESTControl.States.State;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+/**The primary StateMachine for controlling overall Robot movement. 
+ * This StateMachine will require RobotStates, which are states that requires other StateMachines to be in specific states
+ * The method run() must be put in robotPeriodic
+ */
+public class RobotStateMachine extends SubsystemBase  {
+    public static ArrayList<StateMachine> registeredStateMachines = new ArrayList<StateMachine>();
+
+
+    /**
+     * Register a StateMachine with the {@link com.PESTControl.SystemStateMachines.RobotStateMachine RobotStateMachine}.<p>  <strong>All StateMachines MUST be registered in order to work </strong>
+     * @param machineToRegister The StateMachine to register
+     */
+    public static void registerStateMachine(StateMachine machineToRegister){
+        if(!registeredStateMachines.contains(machineToRegister)){
+            registeredStateMachines.add(machineToRegister);
+        }
+    }
+
+    //The method that is periodically called to run all registered StateMachines
+    private static void run(){
+        for (StateMachine stateMachine : registeredStateMachines) {
+            stateMachine.moveToTarget();
+            stateMachine.display();
+        }
+    }
+
+    @Override
+    public void periodic(){
+        run();
+        StateBinder.periodic();
+    }
+    @Override
+    public void simulationPeriodic(){
+        run();
+        StateBinder.periodic();
+    }
+
+    
+    
+    
+}
