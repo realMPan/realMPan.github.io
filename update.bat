e33c9864bed804870f3e67e1837957bbd87f46cb
realMPan <123257843+realMPan@users.noreply.github.com>
2024-04-29 19:10:59 -0400

    Initial attempt at cleanup of host repo

--- update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Publish
-call gradlew.bat publish
-echo Copy
-Xcopy /E /y .\\build\\repos\\releases\\com\\PESTControl .\\pestControl\\repos\\releases\\com\\PESTControl
--- update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./gradlew publish
-cp -rf build/repos/releases/com/PESTControl yagsl/repos/releases
